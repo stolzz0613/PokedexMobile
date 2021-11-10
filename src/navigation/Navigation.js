2f213974724573887ce555c3374c6d983aaf1b94
Andres B <andres.bustos@globant.com>
2021-11-09 23:49:48 -0500

    Screens and bottom navigation

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {Image} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import FavoriteScreen from '../screens/Favorite';
+import PokedexScreen from '../screens/Pokedex';
+import AccountScreen from '../screens/Account';
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Favorite"
+        component={FavoriteScreen}
+        options={{
+          tabBarLabel: 'Favoritos',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Pokedex"
+        component={PokedexScreen}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({color, size}) => renderPokeball(),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={AccountScreen}
+        options={{
+          tabBarLabel: 'Cuenta',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="user" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function renderPokeball() {
+  return (
+    <Image
+      source={require('../assets/pokeball.png')}
+      style={{width: 75, height: 75, top: -15}}
+    />
+  );
+}
